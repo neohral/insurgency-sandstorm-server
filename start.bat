f6bc43350f848f16f5ef57791125b7c2fd12bf79
neohral <honekingu29@gmail.com>
2023-10-16 13:17:37 +0900

    init

---
+++ start.bat
@@ -0,0 +1,12 @@
+InsurgencyServer.exe ^
+Citadel?Scenario=Scenario_Citadel_Checkpoint_Security?MaxPlayers=28 ^
+-Port=4696 ^
+-QueryPort=8796 ^
+-log ^
+-hostname="honekngu server" ^
+-GameStatsToken="" ^
+-GSLTToken="" ^
+-GameStats ^
+-MapCycle ^
+-Mods ^
+-mutators="AllYouCanEat,ISMCGunfighter_Tac,Medicon,zCore,ImprovedAI,MoreAmmo,HealthBarPlus" 
